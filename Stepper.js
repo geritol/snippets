21a1a831638187efd6469aab95146fb032c049b6
Tolnai Gergő <gergo.tolnai@gmail.com>
2016-10-12 19:46:09 +0200

    basic animated react native component

---
+++ Stepper.js
@@ -0,0 +1,57 @@
+/**
+ * Stepper React Native component
+ * children must have flex: 1 styling
+ * receives 'slide' prop and shows that element
+ */
+
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Dimensions,
+  Animated
+} from 'react-native';
+
+let screen = Dimensions.get("window");
+
+class Stepper extends Component {
+
+  SIZE = React.Children.count(this.props.children);
+
+  constructor(props) {
+  super(props);
+    this.state = {
+      position: new Animated.Value(0),
+    };
+  }
+
+  componentWillReceiveProps(nextProps){
+    Animated.spring(
+      this.state.position,
+      {
+        toValue: - screen.width * nextProps.slide,
+        friction: 10
+      }
+    ).start()
+  }
+
+  render() {
+    return (
+      <Animated.View style={[styles.container, {
+        width: screen.width * this.SIZE,
+        left: this.state.position
+      }]}>
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row'
+  }
+});
+
+module.exports = Stepper;
